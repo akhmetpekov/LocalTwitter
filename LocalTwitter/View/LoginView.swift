a3578bafcf5a870b79baf6ac6e6b71ecc0f3cffd
Erik <erik.ahmetpekov@mail.ru>
2022-12-30 19:54:44 +0600

    Authorization view done.

--- LocalTwitter/View/LoginView.swift
+++ LocalTwitter/View/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import PhotosUI
 import Firebase
+import FirebaseFirestore
+import FirebaseStorage
 
 //MARK: - LoginView
 struct LoginView: View {
@@ -18,6 +20,12 @@ struct LoginView: View {
     @State var createAccount: Bool = false
     @State var showError: Bool = false
     @State var errorMessage: String = ""
+    @State var isLoading: Bool = false
+    //MARK: - User Defaults
+    @AppStorage("user_profile_url") var profileURL: URL?
+    @AppStorage("user_name") var userNameStored: String = ""
+    @AppStorage("user_UID") var userUID: String = ""
+    @AppStorage("log_status") var logStatus: Bool = false
     var body: some View {
         VStack(spacing: 10){
             Text("Lets sign you in")
@@ -70,6 +78,9 @@ struct LoginView: View {
         }
         .vAlign(.top)
         .padding(15)
+        .overlay(content: {
+            LoadingView(show: $isLoading)
+        })
         //MARK: - Register View VIA Sheets
         .fullScreenCover(isPresented: $createAccount) {
             RegisterView()
@@ -79,14 +90,29 @@ struct LoginView: View {
     }
     
     func loginUser(){
+        isLoading = true
+        closeKeyboard()
         Task{
             do{
                 try await Auth.auth().signIn(withEmail: emailID, password: password)
                 print("User Found")
+                try await fetchUser()
             }catch{
                 await setError(error)
             }
         }
+    }
+    //MARK: - If User if Found then Fetching User Data From Firestore
+    func fetchUser() async throws{
+        guard let userID = Auth.auth().currentUser?.uid else {return}
+        let user = try await Firestore.firestore().collection("Users").document(userID).getDocument(as: User.self)
+        
+        await MainActor.run(body: {
+            userUID = userID
+            userNameStored = user.username
+            profileURL = user.userProfileURL
+            logStatus = true
+        })
     }
     
     func resetPassword() {
@@ -105,6 +131,7 @@ struct LoginView: View {
         await MainActor.run(body: {
             errorMessage = error.localizedDescription
             showError.toggle()
+            isLoading = false
         })
     }
 }
@@ -122,6 +149,15 @@ struct RegisterView: View {
     @Environment(\.dismiss) var dismiss
     @State var showImagePicker: Bool = false
     @State var photoItem: PhotosPickerItem?
+    @State var showError: Bool = false
+    @State var errorMessage: String = ""
+    @State var isLoading: Bool = false
+    //MARK: - UserDefaults
+    @AppStorage("log_status") var logStatus: Bool = false
+    @AppStorage("user_profile_url") var profileURL: URL?
+    @AppStorage("user_name") var userNameStored: String = ""
+    @AppStorage("user_UID") var userUID: String = ""
+    
     var body: some View {
         VStack(spacing: 10){
             Text("Lets Register\nAccount")
@@ -157,6 +193,9 @@ struct RegisterView: View {
         }
         .vAlign(.top)
         .padding(15)
+        .overlay(content: {
+            LoadingView(show: $isLoading)
+        })
         .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
         .onChange(of: photoItem) { newValue in
             //MARK: - Extracting UIImage From PhotoItem
@@ -172,6 +211,8 @@ struct RegisterView: View {
                 }
             }
         }
+        //MARK: - Displaying Alert
+        .alert(errorMessage, isPresented: $showError, actions: {})
     }
     
     @ViewBuilder
@@ -217,17 +258,55 @@ struct RegisterView: View {
                 .textContentType(.emailAddress)
                 .border(1, .gray.opacity(0.5))
             
-            Button {
-                
-            } label: {
+            Button(action: registerUser){
                 Text("Sign in")
                     .foregroundColor(.white)
                     .hAlign(.center)
                     .fillView(.black)
                     .padding(.top, 10)
             }
-
+            .disabledWithOpacity(userName == "" || userBio == "" || emailID == "" || password == "" || userProfilePicData == nil)
         }
+    }
+    
+    func registerUser(){
+        isLoading = true
+        closeKeyboard()
+        Task{
+            do{
+                //Creating FireBase Account
+                try await Auth.auth().createUser(withEmail: emailID, password: password)
+                //Uploading Profile Photo Into Firebase Storage
+                guard let userUID = Auth.auth().currentUser?.uid else {return}
+                guard let imageData = userProfilePicData else {return}
+                let storageRef = Storage.storage().reference().child("Profile_Images").child(userUID)
+                try await storageRef.putDataAsync(imageData)
+                //Downloading Photot URL
+                let downloadURL = try await storageRef.downloadURL()
+                //Creating a User FireStore Object
+                let user = User(username: userName, userBio: userBio, userBioLink: userBioLink, userUID: userUID, userEmail: emailID, userProfileURL: downloadURL)
+                let _ = try Firestore.firestore().collection("Users").document(userUID).setData(from: user, completion: { error in
+                    if error == nil {
+                        print("Saved Successfully")
+                        userNameStored = userName
+                        self.userUID = userUID
+                        profileURL = downloadURL
+                        logStatus = true
+                    }
+                })
+            } catch {
+                await setError(error)
+            }
+        }
+    }
+    
+    //MARK: - Displaying Errors VIA Alerts
+    func setError(_ error: Error)async{
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+            isLoading = false
+        })
     }
 }
 
@@ -238,6 +317,17 @@ struct LoginView_Previews: PreviewProvider {
 }
 
 extension View {
+    func closeKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    //MARK: - Disabling With Opacity
+    func disabledWithOpacity(_ condition: Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.6 : 1)
+    }
+    
     //MARK: - Custom Horizontal Alignment
     func hAlign(_ alignment: Alignment) -> some View {
         self
